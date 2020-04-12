1720c2f1f50762716e2e57629b5b506390001156
jovan <jova@syncitgroup.com>
2020-04-12 13:17:57 +0200

    - Database connection and retrieving data from tables

---
+++ etc/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$database = [
+    "dbName" => 'test',
+    "host" => "localhost",
+    "user" => "root",
+    "password" => "temp11!!"
+];
+define('DATABASE', $database);
---
+++ src/repository/SchoolRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Repository;
+
+use Core\DBClass;
+use PDO;
+
+require '../etc/config.php';
+
+/**
+ * Class SchoolRepository
+ * @package Repository
+ */
+class SchoolRepository implements RepositoryInterface
+{
+    /**
+     * @param $id
+     *
+     * @return array
+     */
+    public static function getById($id)
+    {
+        $db = DBClass::getInstance();
+        $queryString = "SELECT `school`.`id`, `school`.`name`,
+                        `grading_type`.`type` as `grading_type`,
+                        `output_type`.`type` as `output_type`,
+                        `grading_type`.`description`
+                        FROM `school`
+                        LEFT JOIN `grading_type` ON `school`.`grading_type` = `grading_type`.`id`
+                        LEFT JOIN `output_type` ON `school`.`output_type` = `output_type`.`id`
+                        WHERE `school`.`id` = ?";
+        $stmt = $db->getConnection()->prepare($queryString);
+        $stmt->execute([$id]);
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $data;
+    }
+}
