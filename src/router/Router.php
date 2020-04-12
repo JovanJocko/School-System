e4b3d4219ae8d75cbb4e4ccadcee089dc57cff7a
jovan <jova@syncitgroup.com>
2020-04-12 12:38:43 +0200

    - Router feature

---
+++ src/router/Router.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Router;
+
+/**
+ * Class Router
+ * @package Router
+ */
+class Router
+{
+    /**
+     * @var string HOME Home route
+     */
+    const HOME = '/';
+
+    /**
+     * @var string CONTROLLER_ROUTE Controller namespace
+     */
+    const CONTROLLER_ROUTE = "Controller\\";
+
+    /**
+     * @var string DEFAULT_CONTROLLER Default controller name
+     */
+    const DEFAULT_CONTROLLER = "Home";
+
+    /**
+     * @var array $routes Routes array where we store the various routes defined.
+     */
+    private $routes;
+
+    /**
+     * The methods adds each route defined to the $routes array
+     *
+     * @param $route
+     * @param $controllerName
+     */
+    public function addRoute($route, $controllerName) {
+        $this->routes[$route] = $controllerName;
+    }
+
+    /**
+     * Execute the specified route defined
+     */
+    function execute() {
+        $queryString = $_SERVER['QUERY_STRING'];
+        $pathArray = explode("=", $queryString);
+        $path = self::HOME . $pathArray['0'];
+        $params = $pathArray[1] ? $pathArray[1] : '';
+
+        // Check if the given route is defined, or execute the default '/' home route.
+        if(array_key_exists($path, $this->routes)) {
+            $controllerNameSpace = self::CONTROLLER_ROUTE . $this->routes[$path];
+            $controllerName = $this->routes[$path];
+        } else {
+            $controllerNameSpace = self::CONTROLLER_ROUTE . self::DEFAULT_CONTROLLER;
+            $controllerName = self::DEFAULT_CONTROLLER;
+        }
+
+        if (file_exists('../src/controller/' . $controllerName . '.php')) {
+            $controller = new $controllerNameSpace();
+            $controller->execute($params);
+        } else {
+            echo "No such Controller";
+        }
+
+    }
+}
